337012d4d1de658395cc64efa4f9c565c91d743a
SetOfNull <mnurinissa@gmail.com>
2023-04-19 01:30:06 +0600

    Initial-commit

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,32 @@
 import java.sql.*;
 
-public class Main {
 
+public class Main {
     static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
     static final String USER = "postgres";
      static final String PASS = "n550490162";
-    static final String QUERY = "SELECT id, login, fullname, email, age, gender FROM users";
+    static final String QUERY = "SELECT id,login, fullname, email, age, gender from users order by id";
+
 
 
     public static void main(String[] args) {
         try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
         Statement stmt = conn.createStatement();
         ) {
-            System.out.println("***BEFORE***");
             ResultSet rs= stmt.executeQuery(QUERY);
-
                 while(rs.next()){
-                System.out.print("ID: " + rs.getInt(("id")));
-                System.out.print(", Age: " + rs.getInt(("age")));
-                System.out.print(", Login: " + rs.getString(("login")));
-                System.out.print(", Fullname: " + rs.getString(("fullname")));
-                System.out.print(", Email: " + rs.getString(("email")));
-                System.out.println(", Gender: " + rs.getString(("gender")));
-            }
-            String sql = "DELETE FROM users WHERE id=5";
-            stmt.executeUpdate(sql);
-
-            System.out.println();
-            System.out.println("***AFTER***");
-
-            ResultSet rs1= stmt.executeQuery(QUERY);
-
-            while(rs1.next()){
-                System.out.print("ID: " + rs1.getInt(("id")));
-                System.out.print(", Age: " + rs1.getInt(("age")));
-                System.out.print(", Login: " + rs1.getString(("login")));
-                System.out.print(", Fullname: " + rs1.getString(("fullname")));
-                System.out.print(", Email: " + rs1.getString(("email")));
-                System.out.println(", Gender: " + rs1.getString(("gender")));
+                    System.out.print("ID:" + rs.getString("id") + " Login: " + rs.getString("login") +
+                            " Name: " + rs.getString("fullname") + " Email: " + rs.getString("email") +
+                            "Age: " + rs.getInt("age"));
+                    if (rs.getString("gender").equals("F")){
+                        System.out.println(" Gender: " + Gender.FEMALE);
+                    }else
+                        System.out.println(" Gender: " + Gender.MALE);
             }
 
         }catch(SQLException e){
             e.printStackTrace();
         }
+    }
+}
 
-    }
-    }
